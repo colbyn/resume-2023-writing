8f7764e1797c57577ddc16b1517f93510b6211ec
Colbyn Wadman <hello@colbyn.com>
2023-08-16 21:23:28 -0600

    save

---
+++ scripts/open.sh
@@ -0,0 +1,7 @@
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
+    --silent-launch \
+    --app='http://localhost:8080/' \
+    –-disable-background-mode \
+    --disable-component-extensions-with-background-pages \
+    --disable-extensions \
+    -–disable-background-networking \
